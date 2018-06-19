ae4e595f03af10de8d54dcea34b9aae4e317d4a4
MTM123 <matiss234@gmail.com>
2018-06-19 19:16:39 +0300

    Fixes command. Added README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Nou
+
+This plugin is a meme.
+
+Spigot: https://www.spigotmc.org/resources/nou.57904/
+
+### Features
+* Sends message to players when they join
+* Sends message to players when chat message matches certain pattern
+* oof
+
+### Installation
+1. Download the latest version here: https://www.spigotmc.org/resources/nou.57904/
+2. Put Nou.jar in the plugins folder.
+
+This plugin uses amazing command framework by Aikar.
+Make sure to check it out here: https://github.com/aikar/commands/
--- src/main/kotlin/lv/mtm123/nou/CoreCommands.kt
+++ src/main/kotlin/lv/mtm123/nou/CoreCommands.kt
@@ -1,18 +1,19 @@
 package lv.mtm123.nou
 
 import co.aikar.commands.BaseCommand
-import co.aikar.commands.annotation.CommandAlias
-import co.aikar.commands.annotation.Dependency
-import co.aikar.commands.annotation.Subcommand
+import co.aikar.commands.annotation.*
 import org.bukkit.ChatColor
 import org.bukkit.command.CommandSender
 
-@CommandAlias("nou|no u")
+@CommandAlias("nou")
 class CoreCommands : BaseCommand(){
 
     @Dependency
     lateinit var plugin : Nou
 
+    @Default
+    @CommandAlias("nour")
+    @CommandPermission("nou.reload")
     @Subcommand("reload|r")
     fun onReload(sender : CommandSender){
         plugin.loadPlugin(true)
